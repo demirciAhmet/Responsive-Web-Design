afdcb1da5d43e729eeed376134e99959a411cdb7
terrabladex <ahmetkaandemirci@outlook.com>
2024-05-10 16:40:00 +0300

    add activity 10

---
+++ in-classActivity10/Activity10_Accordion/accordion.js
@@ -0,0 +1,9 @@
+		$(document).ready(function() {
+			$("#accordions").accordion(
+				{
+					collapsible: true
+				}
+			);
+			
+
+		});
---
+++ in-classActivity10/Activity10_Slider/slider.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+        	$("#slider").bxSlider({
+			  auto: true,
+			  minSlides: 1,
+			  maxSlides: 1,
+			  slideWidth: 250,
+			  slideMargin: 10,
+			  randomStart: true,
+			  moveSlides: 1,
+			  autoStart: true,
+			  autoDelay: 3000,
+			  captions: true,
+			  pagerType: 'short'
+			  
+			}); 	
+    	});
